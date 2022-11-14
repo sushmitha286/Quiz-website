1e4356421b43c13613ee37d8a78fca0cea367c7e
SUSHMITHA <76254481+sushmitha286@users.noreply.github.com>
2022-11-14 17:16:16 +0530

    Add files via upload

---
+++ ques.js
@@ -0,0 +1,133 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the maximum length of a Python identifier?",
+    answer: "No fixed Length is specified",
+    options: [
+      "32",
+      "16",
+      "128",
+      "No fixed Length is specified"
+    ]
+  },
+    {
+    numb: 2,
+    question: "print(2**3 + (5 + 6)**(1 + 1))",
+    answer: "129",
+    options: [
+      "129",
+      "8",
+      "121",
+      "None of the Above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How is a code block indicated in Python?",
+    answer: "Indentation",
+    options: [
+      "Brackets",
+      "Indentation",
+      "Key",
+      "None of the Above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following concepts is not a part of Python?",
+    answer: "Pointers",
+    options: [
+      "Loops",
+      "Dynamic Typing",
+      "Pointers",
+      "All of the Above"
+    ]
+  },
+  {
+    num: 6,
+    question: "In which language is Python written?",
+    answer: "C",
+    options: [
+        "C++",
+        "C",
+        "Java",
+        "None of the above"
+    ]
+  },
+  {
+    num: 7,
+    question: "What will be the result of the following expression in Python “2 ** 3 + 5 ** 2”?",
+    answer: "33",
+    options: [
+        "65536",
+        "33",
+        "169",
+        "None of the above"
+
+    ]
+  },
+  {
+    num: 8,
+    question:"Which of the following are valid string manipulation functions in Python?",
+    answer: "All of the above",
+    options:[
+        "count()",
+        "upper()",
+        "strip()",
+        "All of the above"
+    ]
+
+  },
+  {
+    num: 9,
+    question: "Which of the following is not a valid set operation in python?",
+    answer: "None of the Above",
+    options:[
+        "Union",
+        "Intersection",
+        "Difference",
+        "None of the Above"
+    ]
+
+  },
+  {
+    num: 10,
+    question: "As what datatype are the *kwargs stored, when passed into a function?",
+    answer: "Dictionary",
+    options:[
+        "Dictionary",
+        "List",
+        "Tuple",
+        "None of the above"
+
+    ]
+
+  }
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
